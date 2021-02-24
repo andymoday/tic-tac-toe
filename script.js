5c3ce587fea8aeb14ad3d9630abbdce0cf43d2ed
Andy Mopad <andy.mopad@gmail.com>
2021-02-24 18:02:56 +0000

    add update array function and begin play move function

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-let gameBoardModule = (function(){
-    let gameArray = ['X','O','X','O','X','O','X','O','X'];
-    let gameBoard = document.getElementById('gameboard');
+const gameBoardModule = (function(){
+    let gameArray = ['','','','','','','','',''];
+    getGameArray: function getGameArray(){
+        return gameArray;
+    }
+    setGameArray: function setGameArray(i,mark){
+        gameArray[i] = mark;
+        return gameArray;
+    }
     let squares = []
     render: function render() {
         gameArray.forEach(function (element,i) {
-            squares[i] = document.createElement('div');
-            squares[i].setAttribute('class', 'square');
-            squares[i].textContent = gameArray[i]
-            gameBoard.appendChild(squares[i]);
+            squares[i] = document.getElementById(i+1);
+            squares[i].textContent = element;
         });
     }
-    return {render}
+    switchPlayer: function switchPlayer(player) {
+        player = (player === playerOne) ? playerTwo : playerOne;
+        return playMove(player)
+    }
+    const buttons = Array.from(document.getElementsByClassName('square'));
+    playMove: function playMove(player) {
+            buttons.forEach((button) => {
+                button.addEventListener('click', () => {
+                    if (gameArray[button.id -1] !== '') {
+                        return 'error'
+                    } else {
+                        console.log(player.getName())
+                        setGameArray([button.id -1],player.getMark())
+                        render(); 
+                        switchPlayer(player);
+                        console.log('done1')  
+                        return 'done'; 
+                    }
+                })   
+            }) 
+    } 
+    
+    return {getGameArray, 
+            setGameArray, 
+            render,
+            playMove,
+            switchPlayer}
 })();
-//
-//let displayControllerModule = (function {
-//    
-//})();
-//
-//function Player(xxx) {
-//    this.xxx = yyy;
-//}
-//
-//
-//Player.prototype.xxx = function() {
-//    return(this.xxx);
-//}
+
+
+let displayControllerModule = (function() {
+            
+    //on completion of move gameBoardModule.render()
+})();
+
+const Player = (name, mark) => {
+    const getName  = () => name;
+    const getMark = () => mark;
+    return {getName,
+            getMark}
+}
+const playerOne = Player('Player One', 'X')
+const playerTwo = Player('Player Two', 'O')
